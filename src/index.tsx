722a382a356ff28d3ebcdf33081b3f819c0a91c3
Zachary Freed <zachary.freed@softwire.com>
2021-09-08 11:26:54 +0100

    Converted to typescript (and fixed quotes not being quotes)

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+ return <h1>Thi asdasds is my React app!</h1>;
+ }
+
+export interface Props {
+    name: string;
+    enthusiasmLevel?: number;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
